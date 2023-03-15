057eb31abd1be88787a349a0d65c658fa8b70e65
usernameNIK <nik.lishhuk@mail.ru>
2023-03-15 20:09:03 +0300

    initial

---
+++ WebAplicationWithActions.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+namespace WebAplicationWithActions.Tests.Unit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            var action = 10;
+
+            Assert.Equal(1, action);
+
+        }
+    }
+}
